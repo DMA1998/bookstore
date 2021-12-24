0ab1a693b01f435a6c84423ef4de9cd8d1d26722
Dmitriy Mykh <AX5555AC@gmail.com>
2021-12-24 22:23:12 +0200

    Added yaml configuration

--- src/main/java/com/mykh/bookstore/BookstoreApplication.java
+++ src/main/java/com/mykh/bookstore/BookstoreApplication.java
@@ -1,13 +1,26 @@
 package com.mykh.bookstore;
 
+import com.mykh.bookstore.configuration.YAMLConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private YAMLConfig config;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println("using environment: " + config.getEnvironment());
+		System.out.println("name: " + config.getName());
+		System.out.println("enabled:" + config.isEnabled());
+		System.out.println("servers: " + config.getServers());
+	}
 }
---
+++ src/main/java/com/mykh/bookstore/configuration/YAMLConfig.java
@@ -0,0 +1,23 @@
+package com.mykh.bookstore.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+public class YAMLConfig {
+
+    private String name;
+    private String environment;
+    private boolean enabled;
+    private List<String> servers = new ArrayList<>();
+}
